aa5c1a8f70fc5ea36510a970ddf107349775e754
sh310310 <110559375+sh310310@users.noreply.github.com>
2022-10-15 13:27:06 -0700

    third commit

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,21 +2,23 @@ public class Calculate {
 	int a = 10;
 	int b = 23;
 
+
+	//addition 
     public static double add(double num1, double num2){
 		double result = num1+num2;
 		return result;
 	}
-	//Method for subtraction
+	//subtraction
 	public static double substract(double num1, double num2){
 		double result = num1-num2;
 		return result;
 	}
-	//Method for multiplication
+	//multiplication
 	public static double multiply(double num1, double num2){
 		double result = num1*num2;
 		return result;
 	}
-	//Method for division
+	//division
 	public static double divide(double num1, double num2){
 		double result = num1/num2;
 		return result;
