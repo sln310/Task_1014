f88c43067f988088558304f65b3ad01bac324300
sh310310 <110559375+sh310310@users.noreply.github.com>
2022-10-15 01:25:16 -0700

    first commit

---
+++ src/Calculate.java
@@ -0,0 +1,25 @@
+public class Calculate {
+	int a = 10;
+	int b = 23;
+
+    public static double add(double num1, double num2){
+		double result = num1+num2;
+		return result;
+	}
+	//Method for subtraction
+	public static double substract(double num1, double num2){
+		double result = num1-num2;
+		return result;
+	}
+	//Method for multiplication
+	public static double multiply(double num1, double num2){
+		double result = num1*num2;
+		return result;
+	}
+	//Method for division
+	public static double divide(double num1, double num2){
+		double result = num1/num2;
+		return result;
+	
+	}
+}
---
+++ src/main.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+    char operator;
+    double num1;
+    double num2;
+    double result;
+    String aaa = "continue";
+    
+
+    do {
+
+    // create an object of Scanner class
+    System.out.println("Please type the first number");
+    Scanner scan1 = new Scanner(System.in);
+    num1 = scan1.nextDouble();
+
+
+    // ask users to enter operator
+    System.out.println("Choose an operation:" + "\n" + "> Type + for addition" + "\n" + "> Type - for subtraction" + "\n" + "> Type * for multiplication" + "\n" + "> Type / for division");
+    Scanner scan2 = new Scanner(System.in);
+    operator = scan2.next().charAt(0);
+
+    // ask users to enter numbers
+    System.out.println("Please type the secound number");
+    Scanner scan3 = new Scanner(System.in);
+    num2 = scan3.nextDouble();
+
+    switch (operator) {
+
+        //addition 
+        case '+':
+          System.out.println(Calculate.add(num1, num2));
+          break;
+  
+        //subtraction 
+        case '-':
+          System.out.println(Calculate.substract(num1, num2));
+          break;
+  
+        //multiplication 
+        case '*':
+          System.out.println(Calculate.multiply(num1, num2));
+          break;
+  
+        //division 
+        case '/':
+        if (num1 > 0 && num2 > 0) {
+        System.out.println(Calculate.divide(num1, num2));    
+        } else if (num1 == 0 || num2 == 0) {
+        System.out.println("cannot divide by zero");  
+    } 
+    break;
+}
+
+System.out.println("do you want to continue or quit?");
+Scanner scan4 = new Scanner(System.in);
+aaa = scan4.nextLine();
+
+
+} while (aaa.equals("continue"));
+
+System.out.println("Bye!");
+
+
+    }
+}
